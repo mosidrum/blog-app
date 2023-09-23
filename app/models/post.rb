4f6f5ef8bc057b91edc7bd5fc825b0d1256a4b17
mosidrum <mosiokanga@gmail.com>
2023-09-23 19:54:40 +0100

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }

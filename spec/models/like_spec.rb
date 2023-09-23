00e8478004818c421deb46856d086c5125b64644
mosidrum <mosiokanga@gmail.com>
2023-09-23 19:36:00 +0100

    rspec test pass

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'Comment model' do
-    user1 = User.new(name: 'Alice Johnson', photo: 'alice.jpg', bio: 'Photographer and traveler', posts_counter: 0)
-    user2 = User.new(name: 'Bob Smith', photo: 'bob.jpg', bio: 'Food lover', posts_counter: 0)
-    post = Post.new(title: 'Summer Vacation', text: 'Had a great time at the beach!', author: user1, likes_counter: 1,
-                    comments_counter: 1)
+    user1 = User.new(name: 'isaac ayodele', photo: 'mosiron.jpeg', bio: 'musician, programmer and drummer', posts_counter: 0)
+    user2 = User.new(name: 'serina helen', photo: 'serina.jpeg', bio: 'she loves to cook and dance', posts_counter: 0)
+    post = Post.new(title: 'Rain', text: 'I love rain fall', author: user2, likes_counter: 5,
+                    comments_counter: 2)
     like = Like.create(post:, user: user2)
 
     it 'should increase likes count on the post' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,31 +2,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   user = User.new(
-    name: 'John Doe',
-    photo: 'https://example.com/photos/johndoe',
-    bio: 'Bio for John Doe',
-    posts_counter: 2
+    name: 'Me myself',
+    photo: 'https://memyself.com/photos/memyself',
+    bio: 'talk about me',
+    posts_counter: 13
   )
 
-  describe 'custom validations' do
-    it 'validates the presence of the name' do
+  describe 'custom validations checks' do
+    it 'presence of the name' do
       user.name = nil
       expect(user).to_not be_valid
     end
 
-    it 'validates that posts_counter is an integer' do
+    it 'posts_counter is an integer' do
       user.posts_counter = 'not-an-integer'
       expect(user).to_not be_valid
     end
 
-    it 'validates that posts_counter is greater than or equal to zero' do
+    it 'posts_counter is greater than or equal to zero' do
       user.posts_counter = -1
       expect(user).to_not be_valid
     end
   end
 
-  describe 'custom methods' do
-    it 'returns the most recent 3 posts' do
+  describe 'custom methods checks' do
+    it 'checks the most recent 3 posts' do
       expect(user.recent_posts).to eq(user.posts.last(3))
     end
   end

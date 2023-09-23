131ce1f66677ac5d6ff50b1f6515e35206adfe4c
mosidrum <mosiokanga@gmail.com>
2023-09-23 19:23:53 +0100

    restructure files

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'Comment model' do
-    user1 = User.new(name: 'Alice Johnson', photo: 'alice.jpg', bio: 'Photographer and traveler', posts_counter: 0)
-    user2 = User.new(name: 'Bob Smith', photo: 'bob.jpg', bio: 'Food lover', posts_counter: 0)
-    post = Post.new(title: 'Summer Vacation', text: 'Had a great time at the beach!', author: user1, likes_counter: 1,
+    user1 = User.new(name: 'isaac ayodele', photo: 'mosiron.jpeg', bio: 'musician, programmer and drummer', posts_counter: 0)
+    user2 = User.new(name: 'serina helen', photo: 'serina.jpeg', bio: 'she loves to cook and dance', posts_counter: 0)
+    post = Post.new(title: 'Summer Vacation', text: 'Had a great time at the beach!', author: user1, likes_counter: 2,
                     comments_counter: 1)
-    comment = Comment.create(post:, user: user2, text: 'Hi Tom!')
+    comment = Comment.create(post:, user: user2, text: 'serina is a good girl')
 
     it 'should increase comments count on the post' do
       expect do

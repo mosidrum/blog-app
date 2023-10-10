8a399f70ac4d03d7da97da61bc5b08c8a6422498
mosidrum <mosiokanga@gmail.com>
2023-10-11 00:43:05 +0100

    Add n+1 fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.includes(posts: :comments).find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments).find(params[:id])
     @comments = @post.comments
     @user = User.find(params[:user_id])
   end
